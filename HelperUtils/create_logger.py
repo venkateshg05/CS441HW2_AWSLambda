c0ff541493b6eefb70075cb22d09ef20d5ff4528
venkateshg05 <venkatesh_gopal@outlook.com>
2022-11-01 16:36:58 -0500

    (clean) add comments to create_logger

--- HelperUtils/create_logger.py
+++ HelperUtils/create_logger.py
@@ -2,12 +2,15 @@ import logging
 import logging.config
 import yaml
 
-with open('loggingConfig.yaml', 'r') as f:
-    config = yaml.safe_load(f.read())
-    logging.config.dictConfig(config)
-
 
 def create_logger(name):
+    """
+    creates the logger with the parameters defined in loggingConfig.yaml
+    """
+    with open('loggingConfig.yaml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+
     logger = logging.getLogger(name)
     logger.info(f'Setup logger!')
     return logger
